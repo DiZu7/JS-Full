c8b6a839db49ac5b1a1c23079b62c59a56863670
DiZu7 <dianadovhanyuk@gmail.com>
2022-08-12 21:58:43 +0300

    added task2-hw init files

---
+++ lesson13/task2-hw/index.test.js
@@ -0,0 +1,18 @@
+import { calc } from './calculator.js';
+
+it('should get result of addition', () => {
+  const result = calc('1 + 2');
+  expect(result).toEqual('1 + 2 = 3');
+});
+it('should get result of subtraction', () => {
+  const result = calc('1 - 2');
+  expect(result).toEqual('1 - 2 = -1');
+});
+it('should get result of multiplication', () => {
+  const result = calc('1 * 2');
+  expect(result).toEqual('1 * 2 = 2');
+});
+it('should get result of division', () => {
+  const result = calc('1 / 2');
+  expect(result).toEqual('1 / 2 = 0.5');
+});
