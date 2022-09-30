ff89cb60c2e9eab41060ec39e3e03f71e520c61b
DiZu7 <dianadovhanyuk@gmail.com>
2022-09-30 17:14:41 +0300

    changed js

--- lesson21/task10-hw/index.js
+++ lesson21/task10-hw/index.js
@@ -2,8 +2,9 @@ export function manageClasses() {
   document.querySelector('.one').classList.add('selected');
   document.querySelector('.two').classList.remove('selected');
   document.querySelector('.three').classList.remove('three_done');
-  const classFour = document.querySelector('.four');
-  if (classFour.classList.contains('some-class')) {
-    classFour.classList.add('another-class');
+  const listItemFour = document.querySelector('.four');
+
+  if (listItemFour.classList.contains('some-class')) {
+    listItemFour.classList.add('another-class');
   }
 }
