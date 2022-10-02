7dd3538a657c104f3f8c14c0ff5f906ec1793441
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-02 22:01:34 +0300

    changed js

--- lesson22/task5-hw/index.js
+++ lesson22/task5-hw/index.js
@@ -2,6 +2,7 @@ const btnElem = document.querySelector('.single-use-btn');
 
 const consoleLog = () => {
   console.log('clicked');
+  btnElem.removeEventListener('click', consoleLog);
 };
 
-btnElem.addEventListener('click', consoleLog, { once: true });
+btnElem.addEventListener('click', consoleLog);
