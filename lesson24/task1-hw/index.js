a104fdfc6dea155fc20e1f52002a26bfd320ba69
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-13 22:25:51 +0300

    changed js

--- lesson24/task1-hw/index.js
+++ lesson24/task1-hw/index.js
@@ -1,36 +1,35 @@
-// export function getDiff(startDate, endDate) {
-//   startDate.getTime();
-//   endDate.getTime();
-
-//   const days = Math.floor((diff / (1000 * 60 * 60 * 24)) % 30);
-//   const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-//   const minutes = Math.floor((diff / (1000 * 60)) % 60);
-//   const seconds = Math.floor((diff / 1000) % 60);
-
-//   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-// }
-
-// console.log(getDiff(new Date(2022, 8, 1), new Date()));
-
 export function getDiff(startDate, endDate) {
-  const diffTime = new Date(Math.abs(endDate.getTime() - startDate.getTime()));
+  const diffTime = new Date(endDate.getTime() - startDate.getTime());
 
-  const dayTime = 1000 * 60 * 60 * 24;
-  const hourTime = 1000 * 60 * 60;
-  const minTime = 1000 * 60;
-  const secondTime = 1000;
+  const days = Math.floor((diffTime / (1000 * 60 * 60 * 24)) % 30);
+  const hours = Math.floor((diffTime / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diffTime / (1000 * 60)) % 60);
+  const seconds = Math.floor((diffTime / 1000) % 60);
 
-  const days = Math.floor(diffTime / dayTime);
-
-  const hours = Math.floor((diffTime - days * dayTime) / hourTime);
-
-  const mins = Math.floor((diffTime - days * dayTime - hours * hourTime) / minTime);
-
-  const seconds = Math.floor(
-    (diffTime - days * dayTime - hours * hourTime - mins * minTime) / secondTime,
-  );
-
-  return `${days}d ${hours}h ${mins}m ${seconds}s`;
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
 
 console.log(getDiff(new Date(2022, 8, 1), new Date()));
+
+// export function getDiff(startDate, endDate) {
+//   const diffTime = new Date(Math.abs(endDate.getTime() - startDate.getTime()));
+
+//   const dayTime = 1000 * 60 * 60 * 24;
+//   const hourTime = 1000 * 60 * 60;
+//   const minTime = 1000 * 60;
+//   const secondTime = 1000;
+
+//   const days = Math.floor(diffTime / dayTime);
+
+//   const hours = Math.floor((diffTime - days * dayTime) / hourTime);
+
+//   const mins = Math.floor((diffTime - days * dayTime - hours * hourTime) / minTime);
+
+//   const seconds = Math.floor(
+//     (diffTime - days * dayTime - hours * hourTime - mins * minTime) / secondTime,
+//   );
+
+//   return `${days}d ${hours}h ${mins}m ${seconds}s`;
+// }
+
+// console.log(getDiff(new Date(2022, 8, 1), new Date()));
