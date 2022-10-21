843dda20cd8b23ee159ebb00509631928859cef3
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-21 14:50:52 +0300

    test

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 export const pinger = (count, period) => {
+  let i = count;
   console.log('Ping');
 
-  let i = count;
-  
   const interval = setInterval(() => {
     if (--i > 0) {
       console.log('Ping');
@@ -16,4 +15,4 @@ export const pinger = (count, period) => {
 
 // // examples
 pinger(5, 100); // makes 5 writes with 100 ms interval
-pinger(7, 1500); // makes 7 writes with 1500 ms interval
+pinger(7, 150); // makes 7 writes with 1500 ms interval
