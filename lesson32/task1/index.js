ac0dae7080cf4dc075730c9bd72f2013c8a57d8d
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-24 21:54:40 +0300

    added init files

---
+++ lesson32/task1/index.js
@@ -0,0 +1,21 @@
+const getNumber = value =>
+  new Promise(resolve => {
+    resolve(value);
+  });
+
+const asyncNum1 = getNumber(56);
+const asyncNum2 = getNumber(undefined);
+const asyncNum3 = getNumber('10');
+
+const getSum = numbers =>
+  numbers.filter(value => !isNaN(value)).reduce((acc, number) => acc + Number(number), 0);
+
+export const asyncSum = (...asynNumbers) => {
+  return Promise.all(asynNumbers)
+    .then(numbers => getSum(numbers))
+    .catch(() => Promise.reject(new Error("Can't calculate")));
+};
+
+asyncSum(asyncNum1, Promise.reject(new Error('err')), asyncNum3).then(result =>
+  console.log(result),
+);
