43f1e39de6e4cbefa40ab57d974b21b3af86ebc6
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-26 13:36:44 +0300

    added init files

---
+++ lesson33/task1-hw/index.js
@@ -0,0 +1,38 @@
+//  algo
+// function getTasksList
+// call "fetch" with baseUrl
+// use "then" with callback (response => response.json())
+// return fetch
+
+// function getTaskById(taskId)
+// call "fetch" with baseUrl
+// use "then" with callback (response => response.json())
+// use "then" with callback ( filter array of tasks )
+// return fetch
+
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+export function getTasksList() {
+  return fetch(baseUrl).then(response => response.json());
+}
+
+export function getTaskById(taskId) {
+  return fetch(baseUrl)
+    .then(response => response.json())
+    .then(tasksList => tasksList.filter(taskData => taskId === taskData.id));
+}
+
+// export function getTaskById(taskId) {
+//   return getTasksList().then(tasksList => {
+//     return tasksList.filter(taskData => taskId === taskData.id);
+//   });
+// }
+
+// examples
+// getTasksList().then(tasksList => {
+//   console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+// });
+
+// getTaskById('2').then(taskData => {
+//   console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+// });
