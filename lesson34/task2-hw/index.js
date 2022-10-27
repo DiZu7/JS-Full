e42b3823b15c96df3b0e925e0f65f545fec1e488
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-27 22:19:52 +0300

    test

--- lesson34/task2-hw/index.js
+++ lesson34/task2-hw/index.js
@@ -1,20 +1,25 @@
-// create registerBtnElem
-// add listener 'submit' on registerBtnElem with the function onSendData
-// write the function onSendData (request to db with method POST - save data + clean up the input field ), answer - alert with data in object data type
-// check the registerBtnElem on validation (use reportValidity() in form elem)
+// 1. check validation of inputs
+// 1.1. search inputElem (all)
+// 1.2. add eventListener on inputElems (use forEach)
+// 1.3. write function of validation (use = formElem.reportValidity())
+// 2. Register
+// 2.1. add listener on form
+// 2.2. write function of signing up
+// 2.2.1. get data from form (use Object.fromEntries(new FormData(formElem))
+// 2.2.2.write function of sending data to server
+// 2.2.3. use then on promise, return response json, alert response, clear up inputs
 
 const formElem = document.querySelector('.login-form');
 const buttonElem = document.querySelector('.submit-button');
-
 const inputElems = document.querySelectorAll('.form-input');
+
+const baseUrl = 'https://63597995ff3d7bddb9a34f03.mockapi.io/api/v1/signUp';
 
 const onValidate = () => {
   buttonElem.disabled = !formElem.reportValidity();
 };
 
 [...inputElems].forEach(inputElem => inputElem.addEventListener('input', onValidate));
-
-const baseUrl = 'https://63597995ff3d7bddb9a34f03.mockapi.io/api/v1/signUp';
 
 const saveData = data => {
   return fetch(baseUrl, {
