8e90ae65c513f6fc46c02dfcabc6c316386640ab
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-28 23:11:31 +0300

    added init files

---
+++ lesson35/task3/src/scripts/gateways.js
@@ -0,0 +1,17 @@
+export const fetchUserData = userName => {
+  return fetch(`https://api.github.com/users/${userName}`).then(response => {
+    if (response.ok) {
+      response.json();
+    }
+    throw new Error('Failed load data');
+  });
+};
+
+export const fetchRepositories = url => {
+  return fetch(url).then(response => {
+    if (response.ok) {
+      response.json();
+    }
+    throw new Error('Failed load data');
+  });
+};
---
+++ lesson35/task3/src/scripts/index.js
@@ -0,0 +1,36 @@
+import { fetchUserData, fetchRepositories } from './gateways.js';
+import { renderUserData } from './user.js';
+import { renderRepos, cleanList } from './repos.js';
+import { hideSpinner, showSpinner } from './spinner.js';
+
+const defaultUser = {
+  avatar_url: 'https://avatars3.githubusercontent.com/u10002',
+  name: '',
+  location: '',
+};
+
+renderUserData(defaultUser);
+
+const userNameInputElem = document.querySelector('.name-form__input');
+const showUserBtnElem = document.querySelector('.name-form__btn');
+
+const onSearchUser = () => {
+  showSpinner();
+  cleanList();
+  const userName = userNameInputElem.value;
+  fetchUserData(userName)
+    .then(userData => {
+      renderUserData(userData);
+      return userData.repos_url;
+    })
+    .then(url => fetchRepositories(url))
+    .then(reposList => renderRepos(reposList))
+    .catch(error => {
+      alert(error.message);
+    })
+    .finally(() => {
+      hideSpinner();
+    });
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser);
