5d66a6d395345782cec936e840c31a2cd94a6ee0
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-30 15:14:13 +0200

    test

--- lesson36/task1-hw/index.js
+++ lesson36/task1-hw/index.js
@@ -4,19 +4,26 @@
 // do this comands inside Promise.all (iterate users array by map)
 
 export const getUsersBlogs = async users => {
-  const requests = users.map(userId =>
-    fetch(`https://api.github.com/users/${userId}`)
-      .then(response => response.json())
-      .then(userData => userData.blog),
-  );
+  const result = await Promise.all(
+    users.map(userId => {
+      return fetch(`https://api.github.com/users/${userId}`).then(response => response.json());
+    }),
+  ).then(usersData => usersData.map(userData => userData.blog));
 
-  const usersBlogs = await Promise.all(requests)
-    .then(result => result)
-    .catch(error => error);
-
-  return usersBlogs;
+  return result;
+  // const requests = users.map(userId =>
+  //   fetch(`https://api.github.com/users/${userId}`)
+  //     .then(response => response.json())
+  //     .then(userData => userData.blog),
+  // );
+  // const usersBlogs = await Promise.all(requests)
+  //   .then(result => result)
+  //   .catch(error => {
+  //     throw new Error(error);
+  //   });
+  // return usersBlogs;
 };
 
 // examples
-getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+// getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+// getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
