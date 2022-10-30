19c81eb7e46c066a0f408f7398726721ff73ceb0
DiZu7 <dianadovhanyuk@gmail.com>
2022-10-30 15:15:31 +0200

    test

--- lesson36/task1-hw/index.js
+++ lesson36/task1-hw/index.js
@@ -4,24 +4,24 @@
 // do this comands inside Promise.all (iterate users array by map)
 
 export const getUsersBlogs = async users => {
-  const result = await Promise.all(
-    users.map(userId => {
-      return fetch(`https://api.github.com/users/${userId}`).then(response => response.json());
-    }),
-  ).then(usersData => usersData.map(userData => userData.blog));
+  // const result = await Promise.all(
+  //   users.map(userId => {
+  //     return fetch(`https://api.github.com/users/${userId}`).then(response => response.json());
+  //   }),
+  // ).then(usersData => usersData.map(userData => userData.blog));
 
-  return result;
-  // const requests = users.map(userId =>
-  //   fetch(`https://api.github.com/users/${userId}`)
-  //     .then(response => response.json())
-  //     .then(userData => userData.blog),
-  // );
-  // const usersBlogs = await Promise.all(requests)
-  //   .then(result => result)
-  //   .catch(error => {
-  //     throw new Error(error);
-  //   });
-  // return usersBlogs;
+  // return result;
+  const requests = users.map(userId =>
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => response.json())
+      .then(userData => userData.blog),
+  );
+  const usersBlogs = await Promise.all(requests)
+    .then(result => result)
+    .catch(error => {
+      throw new Error(error);
+    });
+  return usersBlogs;
 };
 
 // examples
