31466cd6254982f809b9c236ee7f2b97ae4ce5d3
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-03 16:14:57 +0200

    updated js

--- lesson33/task1-additional/index.js
+++ lesson33/task1-additional/index.js
@@ -12,26 +12,30 @@
 //   4.3. return result array
 // 5. return fetch
 
+const daysAgo = num => {
+  const now = new Date();
+  return new Date(now.setDate(now.getDate() - num));
+};
 
 export const getMostActiveDevs = parameters => {
   const { days, userId, repoId } = parameters;
-  const day = new Date(Date.now()).getDate();
-  const dateInPast = new Date(Date.now()).setDate(day - days);
 
   return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
     .then(response => response.json())
     .then(dataCommits => dataCommits.map(({ commit }) => commit.author))
-    .then(authorsData => authorsData.filter(({ date }) => new Date(date).getTime() >= dateInPast))
+    .then(authorsData =>
+      authorsData.filter(({ date }) => new Date(date).getTime() >= daysAgo(days)),
+    )
     .then(filteredByDateAuthorsInfo => {
       const result = [];
 
-      filteredByDateAuthorsInfo.forEach(({ name, email }) => {
+      filteredByDateAuthorsInfo.map(({ name, email }) => {
         if (result.length === 0) {
           result.push({ count: 1, name, email });
         }
 
         let authorExist = false;
-        result.forEach(authorData => {
+        result.map(authorData => {
           if (authorData.email === email) {
             authorData.count += 1;
             authorExist = true;
