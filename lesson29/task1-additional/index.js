4154749968bf0046237247611c648219b01ef2b5
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-30 23:02:43 +0200

    test

--- lesson29/task1-additional/index.js
+++ lesson29/task1-additional/index.js
@@ -4,7 +4,7 @@
 // algo
 // do callback with delay (use setTimeOUT)
 
-const delay = (delay, callback, context, ...args) => {
+export const delay = (delay, callback, context, ...args) => {
   setTimeout(() => callback.apply(context, [...args]), delay);
 };
 
