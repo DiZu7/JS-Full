3e7fce9e002b71177c79a59d98f6419d717e51bb
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-30 23:01:21 +0200

    test

---
+++ lesson29/task1-additional/index.js
@@ -0,0 +1,15 @@
+// input: number, func, context, any data
+// output: func? undefined
+
+// algo
+// do callback with delay (use setTimeOUT)
+
+const delay = (delay, callback, context, ...args) => {
+  setTimeout(() => callback.apply(context, [...args]), delay);
+};
+
+const sum = (...args) => {
+  console.log([...args].reduce((acc, args) => acc + args, 0));
+};
+
+delay(2000, sum, this, 2, 3, 4);
