afe71932eac33897bf70fcf4993567f1afb33eff
DiZu7 <dianadovhanyuk@gmail.com>
2022-12-01 21:32:13 +0200

    test

--- lesson24/task1-additional/index.js
+++ lesson24/task1-additional/index.js
@@ -1,28 +1,24 @@
 // input: array
 // output: obj
 
+// const formatter = new Intl.DateTimeFormat('en', { month: 'short' });
+
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-export const studentsBirthDays = students => {
-  const studentsObj = students.reduce((acc, student) => {
-    const month = months[new Date(student.birthDate).getMonth()];
-    return { ...acc, [month]: acc[month] ? acc[month].concat(student) : [student] };
-  }, {});
-
-  return Object.entries(studentsObj).reduce((acc, [month, studentsList]) => {
-    return {
-      ...acc,
-      [month]: studentsList
-        .sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
-        .map(({ name }) => name),
-    };
-  }, {});
+const studentsBirthDays = students => {
+  return students
+    .slice()
+    .sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
+    .reduce((acc, { name, birthDate }) => {
+      const month = months[new Date(birthDate).getMonth()];
+      return { ...acc, [month]: acc[month] ? acc[month].concat(name) : [name] };
+    }, {});
 };
 
 // test data
 const students = [
-  { name: 'Tom', birthDate: '01/15/2010' },
   { name: 'Ben', birthDate: '01/17/2008' },
+  { name: 'Tom', birthDate: '01/15/2010' },
   { name: 'Sam', birthDate: '03/15/2010' },
 ];
 
